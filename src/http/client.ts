d5ea56d45a874d01ceccf48ecad0b1d897e2619d
iamabhigupta <abhishekgupta.july@gmail.com>
2024-02-19 20:41:31 +0530

    add auto refresh token

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,3 +1,4 @@
+import { useAuth } from '@/store/use-auth';
 import axios from 'axios';
 
 export const api = axios.create({
@@ -8,3 +9,34 @@ export const api = axios.create({
     Accept: 'application/json',
   },
 });
+
+const refreshToken = async () => {
+  await axios.post(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.status === 401 && !originalRequest._isRetry) {
+      try {
+        const headers = { ...originalRequest.headers };
+        await refreshToken();
+        return api.request({ ...originalRequest, headers });
+      } catch (err) {
+        console.log('Token refresh error', err);
+        useAuth.getState().logout();
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
--- src/layouts/root.tsx
+++ src/layouts/root.tsx
@@ -1,6 +1,7 @@
 import { self } from '@/http/api';
 import { useAuth } from '@/store/use-auth';
 import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 
@@ -15,6 +16,13 @@ const Root = () => {
   const { data, isLoading } = useQuery({
     queryKey: ['self'],
     queryFn: getUser,
+    retry: (failurCount: number, error) => {
+      if (error instanceof AxiosError && error.status === 401) {
+        return false;
+      }
+
+      return failurCount > 3;
+    },
   });
 
   useEffect(() => {
