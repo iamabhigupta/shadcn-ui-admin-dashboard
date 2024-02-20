4f4f4c1582c98616ea4e4b8e4e13d6a4c955c5de
iamabhigupta <abhishekgupta.july@gmail.com>
2024-02-20 20:48:43 +0530

    changes in home page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,6 +11,7 @@ import { useLogout } from '@/hooks/use-logout';
 import { MobileSidebar } from './mobile-sidebar';
 import { Badge } from '@/components/ui/badge';
 import { useAuth } from '@/store/use-auth';
+import { Text } from '../typography';
 
 export const Header = () => {
   const { user } = useAuth();
@@ -20,27 +21,30 @@ export const Header = () => {
     <header className="p-5 flex justify-between items-center">
       <MobileSidebar />
       <Badge className="bg-primary/10 text-primary hover:bg-primary/10">
-        {user?.role === 'admin' ? 'You are an admin' : user?.tenant?.name}
+        {user?.role === 'admin' ? 'You are an admin 🍕' : user?.tenant?.name}
       </Badge>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Avatar className="cursor-pointer">
-            {/* <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" /> */}
-            <AvatarFallback className="bg-primary/10 text-primary">
-              Ab
-            </AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="p-0" align="end">
-          <DropdownMenuItem
-            onClick={() => logoutMutate()}
-            className="cursor-pointer"
-          >
-            <LogOutIcon className="mr-2 h-4 w-4" />
-            <span>Logout</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="flex items-center gap-x-2">
+        <Text variant="p" className="font-semibold">
+          Howdy, {user?.firstName} 😀
+        </Text>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="cursor-pointer">
+              {/* <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" /> */}
+              <AvatarFallback>Ab</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-0" align="end">
+            <DropdownMenuItem
+              onClick={() => logoutMutate()}
+              className="cursor-pointer"
+            >
+              <LogOutIcon className="mr-2 h-4 w-4" />
+              <span>Logout</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </header>
   );
 };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,84 +1,17 @@
-import { Text } from '@/components/typography';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { IconBadge } from '@/components/icon-badge';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Activity, Box, CreditCard, DollarSign, Users } from 'lucide-react';
 import { Overview } from './overview';
 import { RecentSales } from './recent-sales';
 
 const HomePage = () => {
   return (
-    <div>
-      <Text variant="h4">Good morning, Abhishek 🍕</Text>
-      {/* <div className="grid mt-5 gap-5 md:grid-cols-2 lg:grid-cols-4">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Sales</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
-            </svg>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
-            <p className="text-xs text-muted-foreground">
-              +19% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Sales</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
-            </svg>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
-            <p className="text-xs text-muted-foreground">
-              +19% from last month
-            </p>
-          </CardContent>
-        </Card>
-      </div> */}
-      <div className="grid mt-5 gap-5 md:grid-cols-2 lg:grid-cols-4">
+    <>
+      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-            </svg>
+            <IconBadge icon={DollarSign} variant="success" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">$45,231.89</div>
@@ -90,20 +23,7 @@ const HomePage = () => {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-              <circle cx="9" cy="7" r="4" />
-              <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-            </svg>
+            <IconBadge icon={Users} variant="info" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+2350</div>
@@ -115,19 +35,7 @@ const HomePage = () => {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Sales</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <rect width="20" height="14" x="2" y="5" rx="2" />
-              <path d="M2 10h20" />
-            </svg>
+            <IconBadge icon={CreditCard} />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+12,234</div>
@@ -139,18 +47,7 @@ const HomePage = () => {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
+            <IconBadge icon={Activity} variant="warning" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">+573</div>
@@ -171,15 +68,20 @@ const HomePage = () => {
         </Card>
         <Card className="col-span-3">
           <CardHeader>
-            <CardTitle>Recent Sales</CardTitle>
-            <CardDescription>You made 265 sales this month.</CardDescription>
+            <CardTitle>
+              <div className="flex items-center gap-x-2 text-xl">
+                <IconBadge icon={Box} variant="success" />
+                Recent Sales
+              </div>
+            </CardTitle>
+            {/* <CardDescription>You made 265 sales this month.</CardDescription> */}
           </CardHeader>
           <CardContent>
             <RecentSales />
           </CardContent>
         </Card>
       </div>
-    </div>
+    </>
   );
 };
 
