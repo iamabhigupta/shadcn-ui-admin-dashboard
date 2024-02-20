a266adc67eb9c521b16bcd76716da00781efc193
iamabhigupta <abhishekgupta.july@gmail.com>
2024-02-20 22:57:06 +0530

    remove shadow and border

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   return (
     <>
       <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-4">
-        <Card>
+        <Card className="shadow-none border-none">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
             <IconBadge icon={DollarSign} variant="success" />
@@ -21,7 +21,7 @@ const HomePage = () => {
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="shadow-none border-none">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
             <IconBadge icon={Users} variant="info" />
@@ -33,7 +33,7 @@ const HomePage = () => {
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="shadow-none border-none">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Sales</CardTitle>
             <IconBadge icon={CreditCard} />
@@ -45,7 +45,7 @@ const HomePage = () => {
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="shadow-none border-none">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Active Now</CardTitle>
             <IconBadge icon={Activity} variant="warning" />
@@ -59,7 +59,7 @@ const HomePage = () => {
         </Card>
       </div>
       <div className="grid mt-5 gap-5 md:grid-cols-2 lg:grid-cols-7">
-        <Card className="col-span-4 lg:col-span-4">
+        <Card className="col-span-4 lg:col-span-4 shadow-none border-none">
           <CardHeader>
             <CardTitle>Overview</CardTitle>
           </CardHeader>
@@ -67,12 +67,12 @@ const HomePage = () => {
             <Overview />
           </CardContent>
         </Card>
-        <Card className="col-span-4 lg:col-span-3">
+        <Card className="col-span-4 lg:col-span-3 shadow-none border-none">
           <CardHeader>
             <CardTitle>
               <div className="flex items-center gap-x-2 text-xl">
                 <IconBadge icon={Box} variant="success" />
-                Recent Sales
+                <span>Recent orders</span>
               </div>
             </CardTitle>
             {/* <CardDescription>You made 265 sales this month.</CardDescription> */}
