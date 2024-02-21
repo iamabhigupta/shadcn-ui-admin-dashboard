c27c9dd5e897f754c0a460caf86d66bae6691fe9
iamabhigupta <abhishekgupta.july@gmail.com>
2024-02-21 17:05:50 +0530

    fix recent-sales component

--- src/components/home/recent-sales.tsx
+++ src/components/home/recent-sales.tsx
@@ -5,33 +5,33 @@ interface SalesData {
   name: string;
   email: string;
   amount: number;
-  status: 'preparing' | 'on_the_way' | 'delivered';
+  status: 'Preparing' | 'On the way' | 'Delivered';
 }
 
 export function RecentSales() {
   const SaleStatusBadge = ({ status }: { status: string }) => {
     const statusStyles: Record<string, string> = {
-      preparing: 'bg-primary/10 text-primary hover:bg-primary/10',
-      on_the_way: 'bg-cyan-500/10 text-cyan-500 hover:bg-cyan-500/10',
-      delivered: 'bg-green-500/10 text-green-500 hover:bg-green-500/10',
+      Preparing: 'bg-primary/10 text-primary hover:bg-primary/10',
+      'On the way': 'bg-cyan-500/10 text-cyan-500 hover:bg-cyan-500/10',
+      Delivered: 'bg-green-500/10 text-green-500 hover:bg-green-500/10',
     };
 
     const badgeStyle = statusStyles[status] || '';
 
-    return <Badge className={`hidden lg:block ${badgeStyle}`}>Pending</Badge>;
+    return <Badge className={`hidden lg:block ${badgeStyle}`}>{status}</Badge>;
   };
 
   return (
     <div className="space-y-6">
       {salesData.map((sale, index) => (
         <div key={index} className="flex items-center justify-between">
-          <div className="space-y-1">
+          <div className="space-y-1 w-60">
             <p className="text-sm font-medium leading-none">{sale.name}</p>
             <p className="text-sm text-muted-foreground">{sale.email}</p>
           </div>
-          <div className="flex items-center gap-5 justify-between">
+          <div className="lg:flex-1 flex items-center gap-5 justify-between">
             <SaleStatusBadge status={sale.status} />
-            <div className="lg:w-36 text-right font-medium">
+            <div className="text-right font-medium">
               <p>₹{sale.amount.toFixed(2)}</p>
             </div>
           </div>
@@ -52,30 +52,30 @@ const salesData: SalesData[] = [
     name: 'Olivia Martin',
     email: 'olivia.martin@email.com',
     amount: 1999.0,
-    status: 'preparing',
+    status: 'Preparing',
   },
   {
     name: 'Jackson Lee',
     email: 'jackson.lee@email.com',
     amount: 39.0,
-    status: 'on_the_way',
+    status: 'On the way',
   },
   {
     name: 'Isabella Nguyen',
     email: 'isabella.nguyen@email.com',
     amount: 299.0,
-    status: 'preparing',
+    status: 'Preparing',
   },
   {
     name: 'William Kim',
     email: 'will@email.com',
     amount: 99.0,
-    status: 'delivered',
+    status: 'Delivered',
   },
   {
     name: 'Sofia Davis',
     email: 'sofia.davis@email.com',
     amount: 39.0,
-    status: 'delivered',
+    status: 'Delivered',
   },
 ];
